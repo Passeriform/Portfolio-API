71f770f600072732d2362abaa62f96eefba952a0
Passeriform <utbh23041999@gmail.com>
2021-03-31 14:34:31 +0530

    Add cache-manager and update dependencies.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { WorkModule } from './work/work.module';
 import { AboutModule } from './about/about.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -16,8 +17,21 @@ import { AboutModule } from './about/about.module';
       }),
       inject: [ConfigService],
     }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        ttl: configService.get<string>('CACHE_TTL'),
+      }),
+      inject: [ConfigService],
+    }),
     WorkModule,
     AboutModule
   ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
