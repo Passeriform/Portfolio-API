5ef8bb6f7f2d5a8ee8d165c75eb423dd02d77980
Passeriform <utbh23041999@gmail.com>
2021-03-31 15:12:41 +0530

    Fix swagger OpenAPI specification build bug.

--- src/main.ts
+++ src/main.ts
@@ -13,18 +13,18 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
-  /* Causes conflict issues without FastifyV3 https://github.com/nestjs/swagger/issues/844
-  * const options = new DocumentBuilder()
-  *   .setTitle('Portfolio API')
-  *   .setDescription('Simple, minimal, straightforward API as background for Portfolio.')
-  *   .setVersion('1.0')
-  *   .addTag('portfolio')
-  *   .build();
-  *
-  * const document = SwaggerModule.createDocument(app, options);
-  *
-  * SwaggerModule.setup('api', app, document);
-  */
+  // Causes conflict issues without FastifyV3 https://github.com/nestjs/swagger/issues/844
+  const options = new DocumentBuilder()
+    .setTitle('Portfolio API')
+    .setDescription('Simple, minimal, straightforward API as background for Portfolio.')
+    .setVersion('1.0')
+    .addTag('portfolio')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+
 
   const configService: ConfigService = app.get(ConfigService);
 
